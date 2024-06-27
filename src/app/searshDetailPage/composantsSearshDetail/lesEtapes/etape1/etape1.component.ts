d5755f0af835938b1a66e7f4cf20b14fac572447
Haddad aya <haddadaya.aya@gmail.com>
2024-06-27 09:15:43 +0100

    tester

---
+++ src/app/searshDetailPage/composantsSearshDetail/lesEtapes/etape1/etape1.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-etape1',
+  templateUrl: './etape1.component.html',
+  styleUrls: ['./etape1.component.css']
+})
+export class Etape1Component {
+
+}
